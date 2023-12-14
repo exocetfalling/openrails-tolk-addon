4a92c6880facf281d25c87ef3e9874c0822d8f15
exocetfalling <70193215+exocetfalling@users.noreply.github.com>
2023-12-14 17:30:41 +0800

    Added hotkey support.

--- or_sr_test.py
+++ or_sr_test.py
@@ -1,5 +1,8 @@
 import urllib.request, json 
 from cytolk import tolk
+import keyboard
+
+is_active = True
 
 cutoff = 0.00
 regulator = 0.00
@@ -15,26 +18,37 @@ max_value = 0.00
 dec_value = 0.00
 pct_value = 0.00
 
+def on_space():
+    print('space was pressed')
 
-with tolk.tolk():
+    with tolk.tolk():
+        with urllib.request.urlopen("http://localhost:2150/API/CABCONTROLS") as url:
+            data = json.load(url)
+            data_str = str(data)
+            #data_obj = json.loads(data_str)
+            #print(data)
+        #    print()
+
+        # Print data using loop
+        for element in data:
+            for key in element:
+                value = element[key]
+                print(key, ":", value)
+                if key == "TypeName":
+                    if value == "TRAIN_BRAKE":
+                        tolk.speak("TRAIN BRAKE")
+                    elif value == "STEAM_PR":
+                        tolk.speak("STEAM_PRESSURE")
+                    else:
+                        tolk.speak(value)
+                
+                if key == "RangeFraction":
+                    pct_value = round(value * 100)
+                    tolk.speak(str(pct_value))
+                    tolk.speak("percent")
+ 
+keyboard.add_hotkey('space', on_space)
+
+while is_active == True:
     #with urllib.request.urlopen("http://localhost:2150//API/TRAININFO") as url:
-    with urllib.request.urlopen("http://localhost:2150/API/CABCONTROLS") as url:
-        data = json.load(url)
-        data_str = str(data)
-        #data_obj = json.loads(data_str)
-        #print(data)
-    #    print()
-
-
-    # Print data using loop
-    for element in data:
-        for key in element:
-            value = element[key]
-            if key == "TypeName":
-                tolk.speak(value)
-            if key == "RangeFraction":
-                pct_value = round(value * 100)
-                tolk.speak(str(pct_value))
-                tolk.speak("percent")
-#            else:
-#                print(key, ":", element[key])
+    pass
